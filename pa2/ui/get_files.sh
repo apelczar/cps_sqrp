17a2e494573d4cf9e6e79be9f2a564a8972bc80f
Launa Greer <launagreer@uchicago.edu>
2020-02-14 01:05:21 -0600

    Cleaned repo in preparation for project

--- pa2/ui/get_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Getting PA2 files..."
-
-wget -nv -O pa2-files.tgz https://www.classes.cs.uchicago.edu/archive/2020/winter/30122-1/pa2-files.tgz
-tar xvzf pa2-files.tgz
--- pa3/get_files.sh
+++
@@ -1,4 +0,0 @@
-echo "Getting PA3 files..."
-
-wget -nv -O pa3-files.tgz https://www.classes.cs.uchicago.edu/archive/2020/winter/30122-1/pa3-files.tgz
-tar xvzf pa3-files.tgz
