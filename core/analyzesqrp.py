7050edb832edb6b3ca7f6f33d2a5911206f7b30d
Launa Greer <launagreer@uchicago.edu>
2020-02-14 10:27:07 -0600

    Added placeholder files to show proposed file directory in GitHub

---
+++ core/analyzesqrp.py
@@ -0,0 +1,16 @@
+# CAPP 30122: Final Project - CPS SQRP Playground
+# Winter 2020
+# Ali Pelczar, Lily Grier, and Launa Greer
+# 
+# Processes a school quality rating policy (SQRP) for the Chicago Public
+# Schools (CPS) by assigning rating and attainment scores for each high school
+# in the district and then generating a bias score for the SQRP as a whole.
+
+def main():
+    '''
+    The point of entry for the program.
+    '''
+    print("test main")
+
+if __name__ == '__main__':
+    main()
